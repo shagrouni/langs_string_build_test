7d144b20aeb1ca30942c210ae74b7c3b7e59f174
Khaled Shagrouni <shagrouni@gmail.com>
2024-06-03 15:25:34 +0200

    Add files via upload

---
+++ exe/run.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set  num=%1
+cpp_strbld %num%
+rs_strbld %num%
+python py_strbld.py %num%
+go_strbld %num%
+oc_strbld %num%
+c_strbld %num%
+n_strbld %num%
+d_strbld %num%
+julia jl_strbld.jl %num%
+fp_strbld %num%
+cr_strbld %num%
+cs_strbld.exe  %num%
+fs_strbld.exe  %num%
+red_strbld %num%
+v_strbld %num%
+
