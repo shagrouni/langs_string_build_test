21b835738672f1a78b7c25f933c43b2d17b06dfa
Vincent Esche <regexident@gmail.com>
2024-09-19 21:50:24 +0200

    Clean up "rs_strbld.rs" file

--- src/rs_strbld.rs
+++ src/rs_strbld.rs
@@ -1,16 +1,16 @@
 //rustc -C opt-level=3  rs_strbld.rs -o ..\exe\rs_strbld.exe
-use std::fs;
-use std::time::Instant;
 use std::env;
-use std::io::{self, Write};
 use std::fmt::Write as _;
+use std::fs;
+use std::io::{self, Write};
+use std::time::Instant;
 
 fn format_time(time_ms: u128) -> String {
-  let milliseconds = time_ms % 1000;
-  let seconds = (time_ms / 1000) % 60;
-  let minutes = (time_ms / 1000) / 60;
-  
-  format!("{:02}:{:02}:{02}", minutes, seconds, milliseconds)
+    let milliseconds = time_ms % 1000;
+    let seconds = (time_ms / 1000) % 60;
+    let minutes = (time_ms / 1000) / 60;
+
+    format!("{minutes:02}:{seconds:02}:{milliseconds:02}")
 }
 
 pub fn test(num: i64) {
@@ -18,40 +18,37 @@ pub fn test(num: i64) {
     io::stdout().flush().unwrap();
 
     let mut s = String::from("");
-    let now = Instant::now(); 
-    let mut i: i64 = 0; 
-     
+    let now = Instant::now();
+    let mut i: i64 = 0;
+
     for _ in 1..=num {
         i += 1;
-        //s.push_str(&format!(" R {}", i)); 
-        s.push_str(" R "); 
-        write!(s, "{}", i).unwrap();
+        // write!(s, " R {i}").unwrap();
+        s.push_str(" R ");
+        write!(s, "{i}").unwrap();
+    }
 
-     }
-
-    let elapsed = now.elapsed(); 
+    let elapsed = now.elapsed();
     let len = s.len();
 
     let s_time = format_time(elapsed.as_millis());
-    println!("  {} iter {} len {}", s_time, i, len);
+    println!("  {s_time} iter {i} len {len}");
 
     let file_path = "out/rust_output.txt";
     match fs::write(file_path, s) {
         Ok(_) => (),
-        Err(e) => eprintln!("Error saving string to file: {}", e),
-    } 
+        Err(e) => eprintln!("Error saving string to file: {e}"),
+    }
 }
 
-  fn main () {
- 
+fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        eprintln!("Usage: {} <num>", args[0]);
+        eprintln!("Usage: {path} <num>", path = args[0]);
         std::process::exit(1);
     }
 
     let num: i64 = args[1].parse().expect("Invalid input for num");
 
-    test(num);    
-
-  }  
+    test(num);
+}
